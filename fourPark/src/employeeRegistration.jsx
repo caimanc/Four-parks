31ac53a5a4f7198378516c20516d221b2f346e3e
Valentina <valentivelasquezm@gmail.com>
2024-05-13 16:15:13 -0500

    se agregaron estilos en employeeRegistration, y  routerización con Login

--- fourPark/src/employeeRegistration.jsx
+++ fourPark/src/employeeRegistration.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import logo from './assets/img/logo.png';
 
-const employeeRegistration = () => {
+import { useNavigate } from 'react-router-dom';
+
+
+  
+
+const EmployeeRegistration = () => {
+    const navigate = useNavigate(); // Get useNavigate hook instance
+
+  const handleLoginClick = () => {
+    navigate('/inise'); // Navigate to '/inise' when clicked
+  };
     return (
-    <div className="employeeRegistration">
-        <img src={logo} alt="Four Parking logo" />
-        <h2>Regístrate/Funcionario</h2>
-        <form>
-        <input type="email" placeholder="Correo Electrónico" required /><br></br>
-        <input type="text" placeholder="Nombre" required /><br></br>
-        <input type="text" placeholder="Apellido" required /><br></br>
-        <input type="text" placeholder="Código" required /><br></br>
-        <input type="password" placeholder="Contraseña" required /><br></br><br></br>
-        <button type="submit">Regístrate</button>
-        </form>
-        <p>¿Ya tienes una cuenta? <a href="/IniSe.jsx">Inicia sesión</a></p>
-    </div>
+        
+        <div className="employeeRegistration">
+            <div className='logo-container'>
+                <img src={logo} alt="Four Parking logo" className='logo' />
+            </div>
+            <div className='form-container'>
+            <div className='form-content'>
+                <h2>Regístrate/Funcionario</h2>
+                
+                <form>
+                    <input type="email" placeholder="Correo Electrónico" required className='input-field' /><br />
+                    <input type="text" placeholder="Nombre" required className='input-field'/><br />
+                    <input type="text" placeholder="Apellido" required className='input-field' /><br />
+                    <input type="text" placeholder="Código" required /><br className='input-field' />
+                    <input type="password" placeholder="Contraseña" required /><br className='input-field' /><br />
+                    <button type="submit" className='registerF-button'>Regístrate</button>
+                </form>
+               
+                <p>¿Ya tienes una cuenta? <button onClick={handleLoginClick} className='rout-buttonER'>Inicia sesión</button></p>
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default employeeRegistration;
+export default EmployeeRegistration;
+
+
+
--- fourPark/src/main.jsx
+++ fourPark/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './employeeRegistration.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EmployeeRegistration from './employeeRegistration';
+import Inise from './inise';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Router>
+    <Routes>
+      <Route path="/" element={<EmployeeRegistration />} />
+      <Route path="/inise" element={<Inise />} />
+    </Routes>
+  </Router>
+);
